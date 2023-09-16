a5ea519c18d2ff8ee6eed8a6a21eceabd385e57f
vrnprkh <paintingburritos@gmail.com>
2023-09-16 15:33:09 -0400

    Update openAi.js

--- src/utils/api/openAi.js
+++ src/utils/api/openAi.js
@@ -2,11 +2,12 @@ import OpenAI from "openai";
 import { parseArrayToText, parseCode, parseExpl } from "../parsers/parser";
 
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
 
-export async function createExplainationOpenAi(userInput) {
+
+export async function createExplainationOpenAi(userInput, API_KEY = process.env.OPENAI_API_KEY) {
+    const openai = new OpenAI({
+        apiKey: API_KEY
+    });
     const completion = await openai.chat.completions.create({
         messages: [{ role: "system", content: mainPrompt }, {role: "user", content: parseArrayToText(parseCode(userInput))}],
         model: "gpt-3.5-turbo",
