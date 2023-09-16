09646668c2281d2ddecae67cd44daa58e0bcb1a2
vrnprkh <paintingburritos@gmail.com>
2023-09-16 16:54:43 -0400

    types

--- src/utils/parsers/parser.ts
+++ src/utils/parsers/parser.ts
@@ -1,9 +1,21 @@
-export function parseExpl(text) {
+export type Doc = {
+    start: number;
+    end: number;
+    body: string;
+};
+
+export type Line = {
+    lineNumber: number;
+    code: string;
+}
+
+export function parseExpl(text: string) {
     let regex = /(?=\([0-9]+-[0-9]+\))/g;
     return text.split(regex);
 }
 
-export function parseCode(input) {
+
+export function parseCode(input: string) {
     let arr = input.split('\n');
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
@@ -11,13 +23,17 @@ export function parseCode(input) {
     }
     return newArr
 }
-export function parseArrayToText(arrayCode) {
+export function parseArrayToText(arrayCode: Array<Line>) {
     let output = '';
     for(let i = 0; i<arrayCode.length; i++){
-        output += String(arrayCode[i][0]) +' '+ String(arrayCode[i][1]) + '\n';
+        output += String(arrayCode[i].lineNumber) +' '+ String(arrayCode[i].code) + '\n';
     }
     return output;
   }
+
+export function getObjectFromOutput(splitOutputData: Doc) {
+    
+}
 
 // a chunk consists of two numbers [n,m], for start and end inclusive, and non chunked code will
 
