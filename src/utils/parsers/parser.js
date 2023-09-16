5492050b61cb11d7f95e55c51611b564dcbcc217
vrnprkh <paintingburritos@gmail.com>
2023-09-16 12:22:47 -0400

    Update parser.js

--- src/utils/parsers/parser.js
+++ src/utils/parsers/parser.js
@@ -1,6 +1,13 @@
-function parseExpl(text) {
+export function parseExpl(text) {
     let regex = /(?=\([0-9]+-[0-9]+\))/g;
-    return text.split(regex)
+    return text.split(regex);
 }
-export default parseExpl
 
+export function parseCode(input) {
+    let arr = input.split('\n');
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push([i + 1, arr[i]])
+    }
+    return newArr
+}
