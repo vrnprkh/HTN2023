671b2339c8bfaae5e40e4fb4380f89ad16913a1e
vrnprkh <paintingburritos@gmail.com>
2023-09-16 21:22:39 -0400

    Update openAi.ts

--- src/utils/api/openAi.ts
+++ src/utils/api/openAi.ts
@@ -5,17 +5,15 @@ import { mainPrompt } from "../prompts/prompt";
 
 
 
-export async function createExplainationOpenAi(userInput: string, API_KEY: string) {
+export async function createExplainationOpenAi(userInput: string, API_KEY: string, prompt: string = mainPrompt) {
     const openai = new OpenAI({
         apiKey: API_KEY,
         dangerouslyAllowBrowser: true,
     });
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: mainPrompt }, {role: "user", content: parseArrayToText(parseCode(userInput))}],
+        messages: [{ role: "system", content: prompt }, {role: "user", content: parseArrayToText(parseCode(userInput))}],
         model: "gpt-3.5-turbo-16k",
       });
-
-
       let output = getDocFromOutput(parseExpl(completion.choices[0].message.content))
       return output;
 }
