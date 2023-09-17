40cab48598831bfcda39b6a823f3dd96c4975203
Brendan C <115911754+BChharaw@users.noreply.github.com>
2023-09-16 21:17:40 -0400

    ui/ux changes

--- src/components/Blocks/FormBlock/FormBlock.tsx
+++ src/components/Blocks/FormBlock/FormBlock.tsx
@@ -1,5 +1,5 @@
 import "./FormBlock.css";
-
+import banner from "./image2.png";
 export type FormBlockProps = {
   setInputCode: (code: string) => void;
   setApiKey: (key: string) => void;
@@ -9,28 +9,36 @@ export type FormBlockProps = {
 const FormBlock: React.FC<FormBlockProps> = (props: FormBlockProps) => {
   const { setInputCode, setApiKey, onSubmit } = props;
   return (
-    <div className="prompt-box">
-      <div className="input-container">
-        <textarea
-          rows={15}
-          cols={80}
-          placeholder="Enter input code"
-          onChange={(e) => setInputCode(e.target.value)}
-          className="input-field"
-        />
-      </div>
-      <div className="bottom">
+    <div className="parent">
+      <div className="prompt-box">
+        <img src={banner} alt=""></img>
+        <p>
+          A versative GPT powered tool to understand code faster <br></br>
+          Developed by Brendan Chharawala, Johnathon Xie, Varun Parikh, and Zain
+          Salman
+        </p>
         <div className="input-container">
-          <input
-            type="password"
-            placeholder="Enter API key"
-            onChange={(e) => setApiKey(e.target.value)}
+          <textarea
+            rows={15}
+            cols={80}
+            placeholder="Enter input code"
+            onChange={(e) => setInputCode(e.target.value)}
             className="input-field"
           />
         </div>
-        <button className="submit-button" onClick={onSubmit}>
-          Submit
-        </button>
+        <div className="bottom">
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Enter API key"
+              onChange={(e) => setApiKey(e.target.value)}
+              className="input-field"
+            />
+          </div>
+          <button className="submit-button" onClick={onSubmit}>
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
