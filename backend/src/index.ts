59077998577b848ee85e669dec10f4e2e132d7dd
Zain Salman <zainsalman04@gmail.com>
2023-09-16 22:50:07 -0400

    backend working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,14 @@ import express, { Express, NextFunction, Request, Response } from 'express';
 import OpenAI from 'openai';
 import morgan from 'morgan';
 import 'dotenv/config';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import { OpenAIPayload } from './types';
 
 const app: Express = express();
 app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.json());
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -14,19 +18,19 @@ const openai = new OpenAI({
 const PORT = parseInt(process.env.PORT ?? "5000");
 const HOST = process.env.HOST ?? "localhost";
 
-app.use('', (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.authorization) {
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-});
+// app.use('', (req: Request, res: Response, next: NextFunction) => {
+//   if (req.headers.authorization) {
+//     next();
+//   } else {
+//     res.sendStatus(403);
+//   }
+// });
 
 app.get('/mock', (req: Request, res: Response) => {
   res.send("This is a test response for DocUrCode.");
 });
 
-app.post('/openai', async (req: Request, res: Response) => {
+app.post('/openai', bodyParser.json(), async (req: Request, res: Response) => {
   const payload = req.body as OpenAIPayload;
   const data = await openai.chat.completions.create({
     messages: [
@@ -42,6 +46,7 @@ app.post('/openai', async (req: Request, res: Response) => {
     model: "gpt-3.5-turbo-16k",
     temperature: 0,
   });
+  console.log(JSON.parse(JSON.stringify(data)));
   res.send(data);
 });
 
--- frontend/src/utils/api/openAi.ts
+++ frontend/src/utils/api/openAi.ts
@@ -1,21 +1,23 @@
 import OpenAI from "openai";
 import { getDocFromOutput, parseArrayToText, parseCode, parseExpl } from "../parsers/parser";
 import { mainPrompt } from "../prompts/prompt";
-
-
-
+import axios from "axios";
 
 export async function createExplainationOpenAi(userInput: string, API_KEY: string, prompt: string = mainPrompt) {
-    const openai = new OpenAI({
-        apiKey: API_KEY,
-        dangerouslyAllowBrowser: true,
+    let completion: OpenAI.Chat.Completions.ChatCompletion;
+    return axios.post(
+      "http://localhost:4000/openai",
+      {
+        prompt: mainPrompt,
+        code: parseArrayToText(parseCode(userInput)),
+      },
+      {
+        headers: {
+          Authorization: 'Zain',
+        },
+      },
+    ).then((response) => {
+      completion = response.data;
+      return getDocFromOutput(parseExpl(completion.choices[0].message.content));
     });
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: prompt }, {role: "user", content: parseArrayToText(parseCode(userInput))}],
-        model: "gpt-3.5-turbo-16k",
-        temperature: 0,
-      });
-      let output = getDocFromOutput(parseExpl(completion.choices[0].message.content))
-      return output;
 }
-  
